9f9eb039e34d160aa8e7f1d843297a9fd6f23a53
Ivantonic <antonicivan142@gmail.com>
2017-10-31 18:54:01 +0100

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,10 @@
+
+// document.getElementById("logo").addEventListener("mouseover", function() {
+//   document.getElementByTagName("body").style.backgroundImage = "url(entry-background-sa-bojama.jpg)";
+// }, false);
+// document.getElementById("logo").addEventListener("mouseout", function() {
+//    document.getElementByTagName("body").style.backgroundImage = "url(entry-page-background.jpg)";
+// }, false);
+
+
+// document.getElementById("background").style.MozTransition = 'all' + 500 + 'ms'  + 'ease';
